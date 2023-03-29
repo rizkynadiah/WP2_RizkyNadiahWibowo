60f8d2636c88cf723de52f858b0af1fd2ddcdf00
rizkynadiah <kimkiki407@gmail.com>
2023-03-29 12:55:19 +0700

    perubahan isi Contoh1.php

--- Contoh1.php
+++ Contoh1.php
@@ -4,7 +4,7 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>PERKENALAN</h1>";
-        echo "Perkenalkan nama saya Alfi Azizah, biasa dipanggil Alfi. 
-            Saya tinggal di Tambun Bekasi dan menyukai olahraga Bulutangkis.";
+        echo "Perkenalkan nama saya Rizky Nadiah Wibowo, biasa dipanggil Kiki. 
+            Saya tinggal di Tambun dan menyukai pria-pria BTS.";
     }
 }
