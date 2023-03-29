0d477284fffdc53aba33cc887474025f58d3cc40
rizkynadiah <kimkiki407@gmail.com>
2023-03-29 12:30:25 +0700

    menambahkan Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>PERKENALAN</h1>";
+        echo "Perkenalkan nama saya Alfi Azizah, biasa dipanggil Alfi. 
+            Saya tinggal di Tambun Bekasi dan menyukai olahraga Bulutangkis.";
+    }
+}
